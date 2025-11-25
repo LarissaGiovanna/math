9d7dd483c9e358c968d513566e5c3b9eee4ef50f
Gabriel Parméra <grp@cesar.school>
2025-11-25 09:48:02 -0300

    CALMA, DEIXA EU MUDAR MAIS

--- AreaPiscina/lagrange.py
+++ AreaPiscina/lagrange.py
@@ -1,30 +1,31 @@
+from integral import comprimentosX, largurasY
+from sympy import symbols, expand
+
+x_interpolado = 1
 def interpolacao(x, y, x_interpolado):
     n = len(x)
-    resultado = 0
+    result = 0
     for i in range(n):
         termo = y[i]
         for j in range(n):
             if i != j:
                 termo *= (x_interpolado - x[j]) / (x[i] - x[j])
         resultado += termo
-    return resultado
+    return result
     
-from sympy import symbols, expand
 def polinomio(x, y):
     X = symbols('X')
     n = len(x)
-    polinomio = 0
+    Polinomioh = 0
     for i in range(n):
         termo = y[i]
         for j in range(n):
             if i != j:
                 termo *= (X - x[j]) / (x[i] - x[j])
-        polinomio += termo
-    return expand(polinomio)
+        Polinomioh += termo
+    return expand(Polinomioh)
 
-x_interpolado = 1
-print(f"Y interpolado para X={x_interpolado}: {lagrange_interpolacao(comprimentosX, largurasY, x_interpolado)}")
+print(f"Y interpolado para X={x_interpolado}: {interpolacao(comprimentosX, largurasY, x_interpolado)}")
 
 print("Polinômio de Lagrange:")
-print(lagrange_polinomio(comprimentosX, largurasY))
-
+print(polinomio(comprimentosX, largurasY))

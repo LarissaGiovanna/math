d7ca6dd10cf627465ca147d1287cad7abafcbb59
Gabriel Parméra <grp@cesar.school>
2025-11-25 08:32:15 -0300

    Teste envolvendo o git + teste do código

--- AreaPiscina/lagrange.py
+++ AreaPiscina/lagrange.py
@@ -1,7 +1,11 @@
+<<<<<<< HEAD
 from sympy import symbols, expand, sympify
 
 x_interpolado = 1
 def interpolacao(x, y):
+=======
+def interpolacao(x, y, x_interpolado):
+>>>>>>> b735c63 (Refactor Lagrange interpolation functions)
     n = len(x)
     result = 0
     for i in range(n):
@@ -9,6 +13,7 @@ def interpolacao(x, y):
         for j in range(n):
             if i != j:
                 termo *= (x_interpolado - x[j]) / (x[i] - x[j])
+<<<<<<< HEAD
         result += termo
     return result
     
@@ -19,6 +24,13 @@ def polinomio(x, y):
         raise ValueError("Existem valores repetidos em X. Interpolação de Lagrange não permite isso.")
 
 
+=======
+        resultado += termo
+    return resultado
+    
+from sympy import symbols, expand
+def polinomio(x, y):
+>>>>>>> b735c63 (Refactor Lagrange interpolation functions)
     X = symbols('X')
     n = len(x)
     Polinomioh = 0
@@ -32,5 +44,11 @@ def polinomio(x, y):
 
 # print(f"Y interpolado para X={x_interpolado}: {interpolacao(comprimentosX, largurasY)}")
 
+<<<<<<< HEAD
 # print("Polinômio de Lagrange:")
 # print(polinomio(comprimentosX, largurasY))
+=======
+print("Polinômio de Lagrange:")
+print(lagrange_polinomio(comprimentosX, largurasY))
+
+>>>>>>> b735c63 (Refactor Lagrange interpolation functions)

744edce05d42a873d98268b2f3f1d326ee72bc05
LuisHigino <lfhm@cesar.school>
2025-11-26 11:59:37 -0300

    Código do Trapezio

---
+++ AreaPiscina/trapezio.py
@@ -0,0 +1,20 @@
+import os
+os.system('cls')
+
+def trapezio_simples():
+  h = comprimentosX[-1] - comprimentosX[0]
+  Area_Trapezio = (h/2) * (largurasY[0] + largurasY[1])
+  
+  print(Area_Trapezio)
+  
+
+def trapezio_composto():
+  h = (comprimentosX[-1] - comprimentosX[0])/(numeroDePontos-1)
+  y_soma = 0
+  for i in range(1,(numeroDePontos - 1)):
+    y_meios = 2*largurasY[i]
+    y_soma += y_meios
+  Area_Trapezio = (h/2) * (largurasY[0] + y_soma + largurasY[-1])
+
+  print(Area_Trapezio)
+  

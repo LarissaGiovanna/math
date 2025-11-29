9f71d43a2b932fadba0f7a48f2a72dbd5934749b
Gabriel Parméra <grp@cesar.school>
2025-11-29 07:10:49 -0300

    Pontuada a necessidade de mudanças no código em razão de alterações nos códigos

--- AreaPiscina/lagrange.py
+++ AreaPiscina/lagrange.py
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
+import sys, os
+sys.path.append(os.path.dirname(__file__))
+from main import comprimentosX, largurasY
 from sympy import symbols, expand, sympify
 
 x_interpolado = 1
-def interpolacao(x, y):
-=======
+
 def interpolacao(x, y, x_interpolado):
->>>>>>> b735c63 (Refactor Lagrange interpolation functions)
     n = len(x)
     result = 0
     for i in range(n):
@@ -13,24 +13,10 @@ def interpolacao(x, y, x_interpolado):
         for j in range(n):
             if i != j:
                 termo *= (x_interpolado - x[j]) / (x[i] - x[j])
-<<<<<<< HEAD
         result += termo
     return result
-    
+
 def polinomio(x, y):
-
-    #verificao caso exista x repetidos
-    if len(set(x)) != len(x):
-        raise ValueError("Existem valores repetidos em X. Interpolação de Lagrange não permite isso.")
-
-
-=======
-        resultado += termo
-    return resultado
-    
-from sympy import symbols, expand
-def polinomio(x, y):
->>>>>>> b735c63 (Refactor Lagrange interpolation functions)
     X = symbols('X')
     n = len(x)
     Polinomioh = 0
@@ -42,13 +28,7 @@ def polinomio(x, y):
         Polinomioh += termo
     return expand(Polinomioh)
 
-# print(f"Y interpolado para X={x_interpolado}: {interpolacao(comprimentosX, largurasY)}")
+print(f"Y interpolado para X={x_interpolado}: {interpolacao(comprimentosX, largurasY, x_interpolado)}")
 
-<<<<<<< HEAD
-# print("Polinômio de Lagrange:")
-# print(polinomio(comprimentosX, largurasY))
-=======
 print("Polinômio de Lagrange:")
-print(lagrange_polinomio(comprimentosX, largurasY))
-
->>>>>>> b735c63 (Refactor Lagrange interpolation functions)
+print(polinomio(comprimentosX, largurasY))

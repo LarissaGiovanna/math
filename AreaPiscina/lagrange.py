110cc7a48a9c11cffec555d4aa2fa12b00bb9d34
Gabriel Parméra <grp@cesar.school>
2025-12-27 20:04:17 -0300

    Remoção de função duplicada
    Antes havia duas funções que realizavam a mesma atividade, no entanto apenas a segunda é usada. Para otimizar o código removi a função não utilizada.

--- AreaPiscina/lagrange.py
+++ AreaPiscina/lagrange.py
@@ -2,17 +2,6 @@ from sympy import symbols, expand, sympify
 
 x_interpolado = 1
 
-def interpolacao(x, y, x_interpolado):
-    n = len(x)
-    result = 0
-    for i in range(n):
-        termo = y[i]
-        for j in range(n):
-            if i != j:
-                termo *= (x_interpolado - x[j]) / (x[i] - x[j])
-        result += termo
-    return result
-
 def polinomio(x, y):
     X = symbols('X')
     n = len(x)
